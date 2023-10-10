a42d197b6231b342ad7219e93afa11e0a829f5e1
Daniel Onasanya <daniel.juubah@gmail.com>
2023-10-10 15:50:07 -0300

    Project initialization

---
+++ src/main/java/com/api/matchyourpet/MatchyourpetApplication.java
@@ -0,0 +1,13 @@
+package com.api.matchyourpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchyourpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchyourpetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/matchyourpet/MatchyourpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.matchyourpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchyourpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
